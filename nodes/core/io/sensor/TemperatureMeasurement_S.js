bb50e759b6368d03d588ab97c2c196ecbecc745f
maydaycha <maydaychaaaa@gmail.com>
2015-01-09 15:44:37 +0800

    add IASWD, TemperatureMeasurment nodes

---
+++ nodes/core/io/sensor/TemperatureMeasurement_S.js
@@ -0,0 +1,16 @@
+module.exports = function(RED) {
+    function SensorIn(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+
+        this.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+
+        // this.on('close', function () {
+        // });
+    }
+    RED.nodes.registerType("TemperatureMeasurement_S", SensorIn);
+}
